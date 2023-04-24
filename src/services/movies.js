fe76d9f33ffa18b65cb94ababdf408f0e6f5f827
Heidiki <abdelattie@gmail.com>
2023-04-24 09:45:08 +0100

    Refactor API URL handling to use environment variable

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,30 +1,31 @@
 import axios from 'axios';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
 
 class MovieDataService {
   getAll(page =0, limit = 20) {
-    return axios.get(`http://localhost:5000/api/v1/movies?page=${page}&limit=${limit}`);
+    return axios.get(`${API_BASE_URL}/api/v1/movies?page=${page}&limit=${limit}`);
   }
   get(id) {
-    return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`);
+    return axios.get(`${API_BASE_URL}/api/v1/movies/id/${id}`);
   }
   find(query, by = "title", page = 0, limit = 20) {
-    return axios.get(`http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}&limit=${limit}`);
+    return axios.get(`${API_BASE_URL}/api/v1/movies?${by}=${query}&page=${page}&limit=${limit}`);
   }
 
   createReview( data) {
-    return axios.post(`http://localhost:5000/api/v1/movies/review`, data);
+    return axios.post(`${API_BASE_URL}/api/v1/movies/review`, data);
   }
 
   updateReview(data) {
-    return axios.put(`http://localhost:5000/api/v1/movies/review/`, data);
+    return axios.put(`${API_BASE_URL}/api/v1/movies/review/`, data);
   }
 
   deleteReview(id, userId) {
-    return axios.delete(`http://localhost:5000/api/v1/movies/review`, {data: {review_id: id, user_id: userId}});
+    return axios.delete(`${API_BASE_URL}/api/v1/movies/review`, {data: {review_id: id, user_id: userId}});
   }
 
   getRating(id) {
-    return axios.get(`http://localhost:5000/api/v1/movies/ratings`);
+    return axios.get(`${API_BASE_URL}/api/v1/movies/ratings`);
   }
 }
 
